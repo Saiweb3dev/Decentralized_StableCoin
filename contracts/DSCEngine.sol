494d2a1a06179266a6bd03ca73eab76b6093e2ae
saik434455@gmail.com <saik434455@gmail.com>
2024-04-07 21:10:27 +0530

    Collateral function written

--- contracts/DSCEngine.sol
+++ contracts/DSCEngine.sol
@@ -1,12 +1,68 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
+import {DecentralizedStableCoin} from "./DecentralizedStableCoin.sol";
+import {ReentrancyGuard} from "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+contract DSCEngine is ReentrancyGuard {
 
-contract DSCEngine{
+
+    //Errors
+    error DSCEngine__NeedsMoreThanZero();
+    error DSCEngine__TokenAddressesAndPriceFeedsMustBeSameLength();
+    error DSCEngine__NotAllowerToken();
+    error DSCEngine__TransferFailed()
+    
+    //State Variables
+    mapping(address token => address priceFeed) private s_priceFeeds;
+    mapping(address user => mapping(address token => uint amt)) private s_collateralDeposited;
+
+    DecentralizedStableCoin private immutable i_dsc;
+    
+    //Events
+    event CollateralDeposited(address indexed user, address indexed token, uint indexed amount);
+
+
+
+    // Modifiers
+    modifier moreThanZero(uint amt){
+      if(amt == 0){
+        revert DSCEngine__NeedsMoreThanZero();
+      }
+      _;
+    }
+
+    modifier isAllowedToken(address token){
+      if(token == address(0)){
+        revert DSCEngine__NotAllowerToken();
+      }
+      _;
+    }
+    //Functions
+    constructor(address[] memory tokenAddresses,address[] memory priceFeedAddresses,address dscAddress) {
+      if(tokenAddresses.length != priceFeedAddresses.length){
+        revert DSCEngine__TokenAddressesAndPriceFeedsMustBeSameLength();
+      }
+      for(uint i=0;i<tokenAddresses.length;i++){
+        s_priceFeeds[tokenAddresses[i]] = priceFeedAddresses[i];
+      }
+      i_dsc = DecentralizedStableCoin(dscAddress);
+    }
+
+    //External Functions
      function depositCollateralAndMintDsc() external {
       
      }
-     function depositCollateral() external {
- 
+     /*
+     *@param tokenCollateralAddress the address of the collateral token
+     *@param amountCollateral the amount of collateral
+     */
+     function depositCollateral(address tokenCollateralAddress,uint amountCollateral) external moreThanZero(amountCollateral) isAllowedToken(tokenCollateralAddress) nonReentrant {
+       s_collateralDeposited[msg.sender][tokenCollateralAddress] += amountCollateral;
+       emit CollateralDeposited(msg.sender,tokenCollateralAddress,amountCollateral);
+       bool success =IERC20(tokenCollateralAddress).safeTransferFrom(msg.sender,address(this),amountCollateral);
+       if(!success){
+         revert DSCEngine__TransferFailed();
+       }
      }
      function redeemCollateralForDsc() external {
 
--- contracts/DecentralizedStableCoin.sol
+++ contracts/DecentralizedStableCoin.sol
@@ -1,38 +1,35 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
-import {ERC20Burnable,ERC20} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import {ERC20Burnable, ERC20} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 contract DecentralizedStableCoin is ERC20Burnable, Ownable {
+    error DSC__MustBeMoreThanZero();
+    error DSC__BurnAmountExceedsBalance();
+    error DSC__NotZeroAddress();
 
-   error DSC__MustBeMoreThanZero();
-   error DSC__BurnAmountExceedsBalance();
-   error DSC__NotZeroAddress();
+    constructor() ERC20("DecentralizedStableCoin", "DSC") {}
 
-    constructor() ERC20("DecentralizedStableCoin","DSC") {
-
-    }
-
-    function burn(uint _amt) public override onlyOwner{
+    function burn(uint _amt) public override onlyOwner {
         uint balance = balanceOf(msg.sender);
-        if(_amt <= 0){
+        if (_amt <= 0) {
             revert DSC__MustBeMoreThanZero();
         }
-        if(balance < _amt){
+        if (balance < _amt) {
             revert DSC__BurnAmountExceedsBalance();
         }
         super.burn(_amt);
     }
 
-    function mint(address _to,uint _amt) external onlyOwner returns(bool){
-      if(_to == address(0)){
-        revert DSC__NotZeroAddress();
-      }
-      if(_amt <= 0){
-        revert DSC__MustBeMoreThanZero();
-      }
-      _mint(_to,_amt);
-      return true;
+    function mint(address _to, uint _amt) external onlyOwner returns (bool) {
+        if (_to == address(0)) {
+            revert DSC__NotZeroAddress();
+        }
+        if (_amt <= 0) {
+            revert DSC__MustBeMoreThanZero();
+        }
+        _mint(_to, _amt);
+        return true;
     }
 }
