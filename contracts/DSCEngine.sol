5ef13e1533e2223dcc77c1b138f12c03e20c237d
saik434455@gmail.com <saik434455@gmail.com>
2024-04-07 12:33:58 +0530

    DSC initial setup

---
+++ contracts/DSCEngine.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+contract DSCEngine{
+     function depositCollateralAndMintDsc() external {
+      
+     }
+     function depositCollateral() external {
+ 
+     }
+     function redeemCollateralForDsc() external {
+
+     }
+     function redeemCollateral() external {
+
+     }
+     function mintDsc() external {}
+     function burnDsc() external {}
+     function liquidate() external {}
+     function getHealthFactor external view {}
+}
